bc7fa76c5b291b62dad0ff7584f7db6e8265ef41
Nickolay <zylex77@gmail.com>
2020-05-18 23:00:04 +0300

    prepare for Heroku 1

--- src/main/java/com/zylex/carbot/CarbotApplication.java
+++ src/main/java/com/zylex/carbot/CarbotApplication.java
@@ -22,9 +22,9 @@ public class CarbotApplication {
     public static final boolean HEADLESS_DRIVER = false;
 
     public static void main(String[] args) {
-        System.getProperties().put("proxySet", "true");
-        System.getProperties().put("socksProxyHost", "127.0.0.1");
-        System.getProperties().put("socksProxyPort", "9150");
+//        System.getProperties().put("proxySet", "true");
+//        System.getProperties().put("socksProxyHost", "127.0.0.1");
+//        System.getProperties().put("socksProxyPort", "9150");
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
         try {
